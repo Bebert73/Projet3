ea422ad835feacdf0294fdd7601fa7cfa819d06e
Bebert_73 <75555568+Bebert73@users.noreply.github.com>
2022-02-16 16:43:42 +0100

    form update

--- form_add.php
+++ form_add.php
@@ -6,112 +6,116 @@ $db = $database->getConnection();
 
 $sql = "SELECT * FROM accommodation";
 $result = $db->query($sql);
-
 ?>
 
 
 <html>
 <head>
-	<link rel="stylesheet" href="css/styles.css">
+	<meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/styles.css">
 	<title>Ajouter Gite</title>
 </head>
 
+<body>
 <main>
-
-<body class="form_add">
-	<a href="./admin.php">Home</a>
+        <div class="containerlarge">
+            
+	<a href="./admin.php">ADMIN FORM</a>
+	<div class="formulairelarge">
 	<br/><br/>
-	<div class="containeradmin">
-		<div class="formulaireadmin">
-	 <form action="./add_php/add_hebergement.php" method="post" name="form1" enctype="multipart/form-data" >
-		<table>
+	<div id="msg"></div>
+	<form action="./add_php/add_hebergement.php" method="post" enctype="multipart/form-data" name="form1" >
+		<table width="25%" border="0">
 			<tr> 
-				<td>Titre du gite</td>
-				<td><input type="text" name="acc" class="input-form" required></td>
+				<td class="form-text" >Titre</td>
+				<td><input type="text" name="acc"></td>
 			</tr>
 			<tr> 
-				<td>description</td>
-				<td><input type="text" name="desc" class="input-form" required></td>
+				<td class="form-text" >Description</td>
+				<td><input type="text" name="desc"></td>
 			</tr>
 			<tr> 
-				<td>Photo</td>
-				<td><input type="file" name="img" class="input-form" multiple accept="image/png, image/jpg" required></td>
+				<td class="form-text" >Image</td>
+				<td><input type="file" name="img"></td>
 			</tr>
-			
+
 			<tr> 
-				<td>nombres de lits</td>
-				<td><input type="number" name="nb" class="input-form" min="0" required></td>
-			</tr>
-			<tr> 
-				<td>nombres de douches</td>
-				<td><input type="number" name="nbt" class="input-form" required></td>
+				<td class="form-text">lits</td>
+				<td><input type="number" name="nb" min="0"></td>
 			</tr>
 			<tr> 
-				<td>localisation</td>
-				<td><input type="text" name="gl" class="input-form" required></td>
+				<td class="form-text">douches</td>
+				<td><input type="number" name="nbt" min="0"> </td>
 			</tr>
 			<tr> 
-				<td>prix</td>
-				<td><input type="number" name="price" class="input-form" required></td>
+				<td class="form-text">Localisation</td>
+				<td><input type="text" name="gl"></td>
+			</tr>
+			<tr> 
+				<td class="form-text">Prix</td>
+				<td><input type="number" name="price" min="0"></td>
 			</tr>
 				<td></td>
-				<td><input type="submit" name="Submit" value="Ajouter" class="input-form"> </td>
+				<td><input type="submit" name="Submit" value="Ajouter"></td>
 			</tr>
 		</table>
 	</form>
-	 </div>
-	 </main>
-<?php 
-if ($result->rowCount() > 0){
-?> 
-<div class="tableau">
-<table> 
-	<tr>
-		<th colspan="8"><h2> Gite enregister </h2> </th>
-</tr>
-		<t>
-		<th> ID </th>
-		<th> Titre </th>
-		<th> Description </th>
-		<th> Image </th>
-		<th> Lits </th>
-		<th> salle de bains </th>
-		<th> localisation </th>
-		<th> prix </th>
-		<th> delete </th>
 
-</t>
 
-<?php
-while($rows = $result->fetch(PDO::FETCH_ASSOC)) {
-
-	?>
 	
-	<form action="" name="form2">
-<tr>
-<td> <?php echo $rows['id']; ?> </td> 
-<td> <?php echo $rows['accommodation_title']; ?> </td> 
-<td> <?php echo $rows['description']; ?> </td> 
-<td> <?php echo $rows['image']; ?> </td> 
-<td> <?php echo $rows['number_of_beds']; ?> </td> 
-<td> <?php echo $rows['number_of_bathrooms']; ?> </td> 
-<td> <?php echo $rows['geographic_location']; ?> </td> 
-<td> <?php echo $rows['price']; ?> </td> 
-<td><a href="./add_php/delete.php?acco=<?php echo $rows['id'] ?>">supprimer</a></td>
-
-</tr>
-<?php 
-  }
-  echo "</table>";
-} else {
-  echo "0 results";
-}
 
 
-?> 
+
+			<?php 
+			if ($result->rowCount() > 0){
+			?> 
+			<table algin="center" border="1px" style="width:600px"; line-height:40px;> 
+				<tr>
+					<th colspan="8"><h2> Gite enregister </h2> </th>
+			</tr>
+					<t>
+					<th> ID </th>
+					<th> Titre </th>
+					<th> Description </th>
+					<th> image </th>
+					<th> Lits </th>
+					<th> salle de bains </th>
+					<th> localisation </th>
+					<th> prix </th>
+					<th> delete </th>
+
+			</t>
+			<?php
+			while($rows = $result->fetch(PDO::FETCH_ASSOC)) {
+
+				?>
+				
+				<form action="" name="form2">
+			<tr>
+			<td> <?php echo $rows['id']; ?> </td> 
+			<td> <?php echo $rows['accommodation_title']; ?> </td> 
+			<td> <?php echo $rows['description']; ?> </td> 
+			<td> <?php echo $rows['image']; ?> </td> 
+			<td> <?php echo $rows['number_of_beds']; ?> </td> 
+			<td> <?php echo $rows['number_of_bathrooms']; ?> </td> 
+			<td> <?php echo $rows['geographic_location']; ?> </td> 
+			<td> <?php echo $rows['price']; ?> </td> 
+			<td><a class="sup"  href="./add_php/delete.php?acco=<?php echo $rows['id'] ?>">x</a></td>
+			</tr>
+			<?php 
+ 			 }
+			echo "</table>";
+			} else {
+			echo "0 results";
+			}
+			?> 
+</form>
+
+</div>
 </div>
 
-</form>
 </body>
 </html> 
 <!-- form -->
